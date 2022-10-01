2f4471d6cdf2062f5994fd14df4ba295cae7c2a9
Rune Kragh Simonsen <runedimsen@gmail.com>
2022-10-01 13:59:18 +0200

    Added ynab integration

---
+++ src/filter.ts
@@ -0,0 +1,27 @@
+import { Transaction } from "./filewatcher";
+import * as ynab from "ynab"
+
+export function prepareTransactions(foundTransactions: Transaction[], existingTransactions: ynab.TransactionDetail[], accountId: string): ynab.SaveTransaction[] {
+    const result: ynab.SaveTransaction[] = [];
+
+    for (let index = 0; index < foundTransactions.length; index++) {
+        const transaction = foundTransactions[index];
+
+        // If the transaction already is in ynab, skip it
+        if (existingTransactions.some(t => {
+            return t.date === transaction.date &&
+                t.memo == transaction.memo &&
+                t.amount == transaction.amount
+        })) {
+            console.log(`Skipping transaction for date ${transaction.date} with text ${transaction.memo}`);
+            continue;
+        }
+
+        result.push({
+            ...transaction,
+            account_id: accountId
+        });
+    }
+
+    return result;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,48 @@
-console.log("Hello WorldS")
+import * as fs from "node:fs"
+import * as ynab from "ynab";
+import { watchDirectoryForTransactions } from "./filewatcher";
+import { prepareTransactions } from "./filter";
+
+interface AccountConfiguration {
+  bankName: string,
+  ynabId: string
+}
+
+interface Configuration {
+  ynabAccessToken: string,
+  budgetId: string,
+  downloadsFolder: string,
+  accounts: AccountConfiguration[]
+}
+
+(async () => {
+  try {
+    const configContents = fs.readFileSync('configuration.json');
+    const configuration: Configuration = JSON.parse(configContents.toString());
+    const ynabAPI = new ynab.API(configuration.ynabAccessToken);
+
+    for (let i = 0; i < configuration.accounts.length; i += 1) {
+      const account = configuration.accounts[i];
+
+      // Get the CSV File somehow
+      const transactions = await watchDirectoryForTransactions(configuration.downloadsFolder);
+
+      // Get the oldest date in the transaction list, then get all ynab transactions since then
+      const oldestTransaction = transactions.reduce((prev, current) => {
+        return (prev.date < current.date ? prev : current);
+      });
+
+      const ynabTransactions = (await ynabAPI.transactions.getTransactionsByAccount(configuration.budgetId, account.ynabId, oldestTransaction.date)).data;
+
+      const filteredTransactions = prepareTransactions(transactions, ynabTransactions.transactions, account.ynabId);
+
+      await ynabAPI.transactions.createTransactions(configuration.budgetId, {
+        transactions: filteredTransactions
+      });
+    }
+  } catch (e) {
+    // Deal with the fact the chain failed
+  }
+  // `text` is not available here
+})();
+
